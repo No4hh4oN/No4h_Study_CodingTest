2022abd405ec30045a8f99d3f2f4a7931c75c35a
No4h <no4h1209@gmail.com>
2024-12-30 16:01:47 +0900

    No4h

--- codeExercise/src/java_book/Ex03.java
+++ codeExercise/src/java_book/Ex03.java
@@ -7,6 +7,6 @@ public class Ex03 {
 		
 		System.out.println(c1);
 		System.out.println(c2);
-		System.out.printf("%d\n", (int)c2); //printf, 형변환을 이용해서 65 그대로 출력
+		System.out.printf("%d\n", (int)c2); //printf, 형변환을 이용해서 65 그대로 출력 %o -> 8진수  %x -> 16진수
 	}
 }
