4267c089b8210691229bc4e92a6091b69177d66b
No4h <no4h1209@gmail.com>
2024-12-30 17:22:44 +0900

    No4h

--- codeExercise/src/java_jungol/Ex04.java
+++ codeExercise/src/java_jungol/Ex04.java
@@ -14,3 +14,20 @@ public class Ex04 {
 		}
 	}
 }
+
+//import java.io.*;
+//
+//class Main{
+//    public static void main(String args[])throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        int input = Integer.parseInt(br.readLine());
+//        if(input<0){
+//            bw.write(String.valueOf(input)+"\nminus");
+//        }
+//        else{
+//            bw.write(String.valueOf(input));
+//        }
+//        bw.flush();
+//    }
+//}
