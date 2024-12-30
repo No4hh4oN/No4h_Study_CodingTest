a1e1c5f732272dfc069556bb46d6597b39bbb1e8
No4h <no4h1209@gmail.com>
2024-12-30 14:09:02 +0900

    No4h

---
+++ codeExercise/src/java_backjoon/Ex01.java
@@ -0,0 +1,14 @@
+//2741 N찍기
+package java_backjoon;
+import java.util.Scanner;
+
+public class Ex01 {
+	public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i = 1; i <= n; i++){
+            System.out.println(i);
+        }
+    }
+}
