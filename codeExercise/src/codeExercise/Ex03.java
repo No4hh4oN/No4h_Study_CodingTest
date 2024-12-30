24f78c760c4e1bfbf9694b0b1d1e8b600a38de21
No4h <no4h1209@gmail.com>
2024-12-30 12:47:21 +0900

    No4h

---
+++ codeExercise/src/codeExercise/Ex03.java
@@ -0,0 +1,20 @@
+package codeExercise;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		byte var1 = -128;
+		byte var2 = -30;
+		byte var3 = 0;
+		byte var4 = 30;
+		byte var5 = 127;
+		//byte var6 = 128; 바이트로 표현 불가한 정수, 값의 손실이 발생
+		byte var6 = (byte)128; //명시적 형변환
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+		System.out.println(var6);
+	}
+}
