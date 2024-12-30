72b1dee041b2b0213dbc080ec366ad3276593706
No4h <no4h1209@gmail.com>
2024-12-30 16:42:35 +0900

    No4h

--- codeExercise/src/java_backjoon/Ex02.java
+++ codeExercise/src/java_backjoon/Ex02.java
@@ -9,6 +9,7 @@ public class Ex02 {
 		int sc = sc2.nextInt();
 		sc2.close();
 
+		//If 조건문을 이용한 계산
 		if (sc <= 100 && sc >= 90) {
 			System.out.println("A");
 		} else if (sc >= 80) {
@@ -20,5 +21,24 @@ public class Ex02 {
 		} else {
 			System.out.println("F");
 		}
+		
+//		//Switch-Case를 이용한 계산
+//		switch(sc/10) {
+//		case 9: case 10:
+//			System.out.println("A");
+//			break;
+//		case 8:
+//			System.out.println("B");
+//			break;
+//		case 7:
+//			System.out.println("C");
+//			break;
+//		case 6:
+//			System.out.println("D");
+//			break;
+//		default :
+//			System.out.println("F");
+//			break;
+//		}
 	}
 }
