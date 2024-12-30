725bd50153b5579f8a176953c49b7723ef289085
No4h <no4h1209@gmail.com>
2024-12-30 14:41:14 +0900

    No4h

--- codeExercise/src/java_backjoon/Ex01.java
+++ codeExercise/src/java_backjoon/Ex01.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class Ex01 {
 	public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); //Scanner는 닫아주지 않으면 계속 입력을 받는 상태임, 그래서 close 해줘야함
         int n = sc.nextInt();
+        sc.close(); //이렇게 닫아주면 됨
         
         for(int i = 1; i <= n; i++){
             System.out.println(i);
