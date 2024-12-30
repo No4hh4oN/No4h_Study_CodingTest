6c06b7b1bc4bfcb75883b860884e3ee333feb61d
No4h <no4h1209@gmail.com>
2024-12-30 15:44:35 +0900

    No4h

---
+++ codeExercise/src/java_book/Ex03.java
@@ -0,0 +1,12 @@
+package java_book;
+
+public class Ex03 {
+	public static void main(String[] args){
+		char c1 = 'A';
+		char c2 = 65;
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.printf("%d\n", (int)c2); //printf, 형변환을 이용해서 65 그대로 출력
+	}
+}
