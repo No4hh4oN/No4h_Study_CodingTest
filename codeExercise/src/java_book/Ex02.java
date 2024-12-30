fc7d0e79ce26784616948d154a2cc195d328c3b5
No4h <no4h1209@gmail.com>
2024-12-30 15:39:13 +0900

    No4h

--- codeExercise/src/java_book/Ex02.java
+++ codeExercise/src/java_book/Ex02.java
@@ -1,14 +1,28 @@
+//package java_book;
+//
+//public class Ex02 {
+//	public static void main(String[] args){
+//		int var1 = 10;
+//		int var2 = 012; //8진수
+//		int var3 = 0xA; //16진수
+//		
+//		System.out.println(var1);
+//		System.out.println(var2);
+//		System.out.println(var3);
+//	}
+//
+//}
 package java_book;
 
 public class Ex02 {
 	public static void main(String[] args){
-		int var1 = 10;
-		int var2 = 012; //8진수
-		int var3 = 0xA; //16진수
+		byte var1 = 125;
+		int var2 = 125;
+		for (int i = 0; i < 5; i++) {
+			var1++;
+			var2++;
+			System.out.println("var1 : "+var1+ "\t" + "var2 : "+var2);
+		}
 		
-		System.out.println(var1);
-		System.out.println(var2);
-		System.out.println(var3);
 	}
-
 }
