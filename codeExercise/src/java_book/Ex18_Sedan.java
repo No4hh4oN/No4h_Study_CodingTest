46adee4ee2303ce11dbd289d6ff6a515cc0b6f83
No4h <no4h1209@gmail.com>
2024-12-31 15:26:02 +0900

    No4h

---
+++ codeExercise/src/java_book/Ex18_Sedan.java
@@ -0,0 +1,11 @@
+package java_book;
+
+public class Ex18_Sedan {
+	public static void main(String[] args) {
+		Ex18_Tire compoTire = new Ex18_Tire();
+		compoTire.setSize(20);
+
+		System.out.println(compoTire.getSize());
+	}
+}
+
---
+++ codeExercise/src/java_book/Ex18_Tire.java
@@ -0,0 +1,13 @@
+package java_book;
+
+public class Ex18_Tire {
+	private int size;
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+}
