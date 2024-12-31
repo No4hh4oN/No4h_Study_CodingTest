0475dfc3a9f7eb4c72541798fa993e6fb9a37683
No4h <no4h1209@gmail.com>
2024-12-31 10:21:12 +0900

    No4h

--- codeExercise/src/java_jungol/Ex05.java
+++ codeExercise/src/java_jungol/Ex05.java
@@ -1,22 +1,23 @@
+// 정올-반복제어문1-자가진단1 #536
 package java_jungol;
 
 public class Ex05 {
 	public static void main(String[] args) {
 		int n = 1;
-		//while문
+		// while문
 		while (n < 16) {
 			System.out.print(n + " ");
 			n++;
 		}
-		//for문
+		// for문
 		for (int i = 1; i < 16; i++) {
 			System.out.print(i + " ");
 		}
-		//do while문
+		// do while문
 		int m = 0;
 		do {
 			m++;
 			System.out.print(m + " ");
-		} while(m < 15);
+		} while (m < 15);
 	}
 }
