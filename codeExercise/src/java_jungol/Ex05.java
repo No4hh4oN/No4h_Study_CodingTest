4a1335ba455a61de90c2812d82706bd75897064b
No4h <no4h1209@gmail.com>
2024-12-31 09:21:08 +0900

    No4h

--- codeExercise/src/java_jungol/Ex05.java
+++ codeExercise/src/java_jungol/Ex05.java
@@ -3,9 +3,20 @@ package java_jungol;
 public class Ex05 {
 	public static void main(String[] args) {
 		int n = 1;
-		while(n < 16) {
-			System.out.print(n+" ");
+		//while문
+		while (n < 16) {
+			System.out.print(n + " ");
 			n++;
 		}
+		//for문
+		for (int i = 1; i < 16; i++) {
+			System.out.print(i + " ");
+		}
+		//do while문
+		int m = 0;
+		do {
+			m++;
+			System.out.print(m + " ");
+		} while(m < 15);
 	}
 }
