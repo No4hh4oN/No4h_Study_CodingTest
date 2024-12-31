e5f4a229932d016c1e25899fc99cf299b33bbb8c
No4h <no4h1209@gmail.com>
2024-12-31 10:06:12 +0900

    No4h

---
+++ codeExercise/src/java_book/Ex06.java
@@ -0,0 +1,12 @@
+package java_book;
+
+public class Ex06 {
+	public static void main() {
+		short s = 100;
+		// short result1 = -s; 컴파일 에러
+		int result2 = -s;
+
+		System.out.println(s);
+		System.out.println(result2);
+	}
+}
