69a003ead076f463d0cc840b55fe931660046881
No4h <no4h1209@gmail.com>
2024-12-31 10:16:38 +0900

    No4h

---
+++ codeExercise/src/java_book/Ex07.java
@@ -0,0 +1,11 @@
+package java_book;
+
+public class Ex07 {
+	public static void main(String[] args) {
+		int c = 7;
+		double d  = 4.8;
+		
+		double result1 = c + d;
+		System.out.println(result1);
+	}
+}
