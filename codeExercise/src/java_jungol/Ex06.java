e260c020d9b50be053f81d70ae7056a7afc3c18d
No4h <no4h1209@gmail.com>
2024-12-31 11:12:18 +0900

    No4h

--- codeExercise/src/java_jungol/Ex06.java
+++ codeExercise/src/java_jungol/Ex06.java
@@ -11,9 +11,19 @@ public class Ex06 {
 		for (int i = 0; i < 10; i++) {
 			arr[i] = sc.next();
 		}
-		sc.close();
 		for (int i = 0; i < 10; i++) {
 			System.out.print(arr[i]);
+		}
+		
+		//강사님 코드
+		char[] ar = new char[10];
+		for(int i = 0; i < ar.length; i++) {
+			ar[i] = sc.next().charAt(0);
+		}
+		sc.close();
+		
+		for(int i = 0; i < ar.length; i++) {
+			System.out.print(ar[i]);
 		}
 	}
 }
