76723fae9f71736dc122298a0a83498fae8cadf5
No4h <no4h1209@gmail.com>
2025-01-01 11:47:31 +0900

    No4h

---
+++ codeExercise/src/java_backjoon/Ex03.java
@@ -0,0 +1,14 @@
+package java_backjoon;
+
+import java.util.Scanner;
+
+public class Ex03 {
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int N = sc.nextInt();
+	    sc.close();
+	    for(int i = 0; i < N; i++) {
+	        System.out.println(N-i);
+	    }
+	}
+}
