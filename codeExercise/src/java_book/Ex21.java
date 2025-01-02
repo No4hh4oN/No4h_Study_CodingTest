a68a00427b29fa1be327121d896039dfe2c2d706
No4h <no4h1209@gmail.com>
2025-01-02 10:23:48 +0900

    No4h

---
+++ codeExercise/src/java_book/Ex21.java
@@ -0,0 +1,27 @@
+package java_book;
+
+public class Ex21 {
+	public static void main(String[] args) {
+		A a = new A();
+		a.methodA();
+		
+		B b = new B();
+		b.methodA();
+		b.methodB();
+	}
+}
+
+class A {
+	int fieldA = 2;
+	
+	void methodA() {
+		System.out.println(fieldA);
+	}
+}
+
+class B extends A { // A 상속받음
+	int fieldB = 3;
+	void methodB() {
+		System.out.println(fieldB);
+	}
+}
