52017f6bcd045337cd9de9125b0a1b4ced9d1655
No4h <no4h1209@gmail.com>
2025-01-03 16:07:31 +0900

    No4h

--- codeExercise/src/java_jungol/Ex13.java
+++ codeExercise/src/java_jungol/Ex13.java
@@ -11,3 +11,24 @@ public class Ex13 {
 		System.out.printf("%.6f", weight);
 	}
 }
+
+//public class Ex13 {
+//    public static void main(String[] args) {
+//        int height = 170;
+//        double weight = 68.6;
+//
+//        System.out.println("My height");
+//        printHeight(height);
+//
+//        System.out.println("My weight");
+//        printWeight(weight);
+//    }
+//    
+//    public static void printHeight(int height) {
+//        System.out.printf("%d\n", height);
+//    }
+//
+//    public static void printWeight(double weight) {
+//        System.out.printf("%.6f\n", weight);
+//    }
+//}
