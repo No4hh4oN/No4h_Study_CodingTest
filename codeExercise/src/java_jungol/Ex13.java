98104a3bf69d634616a9ed719fee4cc7ffd5d593
No4h <no4h1209@gmail.com>
2025-01-03 15:42:51 +0900

    No4h

---
+++ codeExercise/src/java_jungol/Ex13.java
@@ -0,0 +1,13 @@
+// 정올-출력-자가진단6 #506
+package java_jungol;
+
+public class Ex13 {
+	public static void main(String[] args) {
+		int height = 170;
+		double weight = 68.6;
+		System.out.println("My height");
+		System.out.println(height);
+		System.out.println("My weight");
+		System.out.printf("%.6f", weight);
+	}
+}
