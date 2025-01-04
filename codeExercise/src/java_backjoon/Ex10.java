c97b7952f87fb5f7c1b1a224ead356d30cc033e1
No4h <no4h1209@gmail.com>
2025-01-04 13:33:15 +0900

    No4h

---
+++ codeExercise/src/java_backjoon/Ex10.java
@@ -0,0 +1,20 @@
+// 백준-10039-평균 점수
+package java_backjoon;
+
+import java.util.Scanner;
+
+public class Ex10 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] stuList = new int[5];
+		int avg = 0;
+		for(int i = 0; i < stuList.length; i++) {
+			stuList[i] = sc.nextInt();
+			if(stuList[i] < 40) { 
+				stuList[i] = 40;
+			}
+			avg += stuList[i];
+		}
+		System.out.println(avg/stuList.length);
+	}
+}
