48a081eaebb18ffc6234d64bd3658112feaed490
No4h <no4h1209@gmail.com>
2025-01-06 17:32:17 +0900

    No4h

---
+++ codeExercise/src/java_jungol/Ex17.java
@@ -0,0 +1,18 @@
+// 정올-선택제어문-자가진단2 #529
+package java_jungol;
+
+import java.util.Scanner;
+
+public class Ex17 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BMI(sc.nextInt(), sc.nextInt());
+	}
+	
+	static void BMI(int n1, int n2) {
+		System.out.println(n2 + 100 - n1);
+		if (n2 + 100 - n1 > 0) {
+			System.out.println("Obesity");
+		}
+	}
+}
