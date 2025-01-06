32f8bea8015eb99328bc1452b62496b17c694e97
No4h <no4h1209@gmail.com>
2025-01-06 17:46:48 +0900

    No4h

---
+++ codeExercise/src/java_backjoon/Ex14.java
@@ -0,0 +1,17 @@
+// 백준-곱셈-2588
+package java_backjoon;
+
+import java.util.Scanner;
+
+public class Ex14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        
+        System.out.println(n1 * (n2 % 10));
+        System.out.println(n1 * ((n2-(n2/100)*100)/10));
+        System.out.println(n1 * (n2 / 100));
+        System.out.println(n1 * n2);
+    }
+}
