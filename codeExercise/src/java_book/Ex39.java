c294edfdccd5e67a854d9134a276de822bed60dc
No4h <no4h1209@gmail.com>
2025-01-07 14:36:43 +0900

    No4h

---
+++ codeExercise/src/java_book/Ex39.java
@@ -0,0 +1,9 @@
+package java_book;
+
+public class Ex39 {
+
+	public static void main(String[] args) {
+		String javaHome = System.getenv("Path");
+		System.out.println("Path : " + javaHome);
+	}
+}
