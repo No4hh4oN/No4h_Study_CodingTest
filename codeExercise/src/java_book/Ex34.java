a7b3253ab45a1aa6ad90d258487e8bf9d8ab1911
No4h <no4h1209@gmail.com>
2025-01-07 10:33:08 +0900

    No4h

---
+++ codeExercise/src/java_book/Ex34.java
@@ -0,0 +1,15 @@
+package java_book;
+
+import java.util.Date;
+
+public class Ex34 {
+	public static void main(String[] args) {
+		Object obj1 = new Object();
+		Date obj2 = new Date();
+		
+		System.out.println(obj1.toString());
+		System.out.println(obj2.toString());
+		System.out.println(obj1);
+		System.out.println(obj2);
+	}
+}
