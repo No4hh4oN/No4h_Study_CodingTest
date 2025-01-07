2435b9f67490a7d215ce377838f06dbaf638eba7
No4h <no4h1209@gmail.com>
2025-01-07 09:47:30 +0900

    No4h

--- codeExercise/src/java_backjoon/Ex14.java
+++ codeExercise/src/java_backjoon/Ex14.java
@@ -8,6 +8,7 @@ public class Ex14 {
         Scanner sc = new Scanner(System.in);
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
+        sc.close();
         
         System.out.println(n1 * (n2 % 10));
         System.out.println(n1 * ((n2-(n2/100)*100)/10));
