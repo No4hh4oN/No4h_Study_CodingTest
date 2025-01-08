737c63f73453fa0b7aeff7138ee6984248363eae
No4h <no4h1209@gmail.com>
2025-01-08 09:42:16 +0900

    No4h

---
+++ codeExercise/src/java_jungol/Ex21.java
@@ -0,0 +1,33 @@
+package java_jungol;
+
+public class Ex21 {
+	public static void main(String[] args) {
+		City[] cities = { 
+				new City("Seoul", "10,312,545", "+91,375"),
+				new City("Pusan", "3,567,910", "+5,868"),
+				new City("Incheon", "2,758,296", "+64,888"), 
+				new City("Daegu", "2,511,676", "+17,230"),
+				new City("Gwangju ", "1,454,636", "+29,774")
+		};
+		
+		for(City city : cities) {
+			city.printInfo();
+		}
+	}
+}
+
+class City {
+	private String name;
+	private String population;
+	private String growth;
+
+	public City(String name, String population, String growth) {
+		this.name = name;
+		this.population = population;
+		this.growth = growth;
+	}
+
+	public void printInfo() {
+		System.out.printf("%15s%15s%15s\n", name, population, growth);
+	}
+}
