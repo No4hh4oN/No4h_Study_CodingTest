5f4d5fcf4b4d96383415753f34cff76890c8b0fe
No4h <no4h1209@gmail.com>
2025-01-08 12:46:31 +0900

    No4h

--- codeExercise/src/java_jungol/Ex21.java
+++ codeExercise/src/java_jungol/Ex21.java
@@ -1,16 +1,22 @@
 // 정올-출력-형성평가5 #105
 package java_jungol;
 
+import java.text.DecimalFormat;
+
 public class Ex21 {
 	public static void main(String[] args) {
+		//3자리마다 , 찍어주는 DecimalFormat
+		DecimalFormat df = new DecimalFormat("###,###");
+		
 		City[] cities = { 
-				new City("Seoul", "10,312,545", "+91,375"),
-				new City("Pusan", "3,567,910", "+5,868"),
-				new City("Incheon", "2,758,296", "+64,888"), 
-				new City("Daegu", "2,511,676", "+17,230"),
-				new City("Gwangju ", "1,454,636", "+29,774")
+				new City("Seoul", df.format(10312545), df.format(91375)),
+				new City("Pusan", df.format(3567910), df.format(5868)),
+				new City("Incheon", df.format(2758296), df.format(64888)), 
+				new City("Daegu", df.format(2511676), df.format(17230)),
+				new City("Gwangju", df.format(1454636), df.format(29774))
 		};
 		
+		//향상된 for 문
 		for(City city : cities) {
 			city.printInfo();
 		}
@@ -25,7 +31,7 @@ class City {
 	public City(String name, String population, String growth) {
 		this.name = name;
 		this.population = population;
-		this.growth = growth;
+		this.growth = "+"+ growth;
 	}
 
 	public void printInfo() {
