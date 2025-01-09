52f33b06fa424ade620eacf7ff91895246616407
No4h <no4h1209@gmail.com>
2025-01-09 14:02:48 +0900

    No4h

---
+++ codeExercise/src/java_backjoon/Ex25.java
@@ -0,0 +1,28 @@
+package java_backjoon;
+
+import java.util.Scanner;
+
+public class Ex25 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		
+		int[] arr = new int [N];
+		int index = 0;
+		for(int i = 0; i < N; i++) {
+			int num = sc.nextInt();
+			if(num < X) {
+				arr[index++] = num;
+			}
+		}
+		sc.close();
+		
+		for(int i = 0; i< arr.length; i++) {
+			if(arr[i] == 0) 
+				break;
+			System.out.print(arr[i] + " ");
+		}
+	}	
+}
