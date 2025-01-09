d8ad3c07f788209a169c2aea815b21b77490e5f3
No4h <no4h1209@gmail.com>
2025-01-09 17:16:11 +0900

    No4h

--- codeExercise/src/java_jungol/Ex22.java
+++ codeExercise/src/java_jungol/Ex22.java
@@ -7,41 +7,39 @@ public class Ex22 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int s, e;
-		
+
+		// 입력
 		while (true) {
 			s = sc.nextInt();
 			e = sc.nextInt();
-			if ((s >= 2 && s <= 9) && (e >= 2 && e <= 9) ) {
+			if ((s >= 2 && s <= 9) && (e >= 2 && e <= 9)) {
 				break;
-			}
-			else {
+			} else {
 				System.out.println("INPUT ERROR!");
 			}
 		}
 		sc.close();
-		
-		for(int i = 1; i < 10; i++) {
-			if(s > e) {
-				for(int j = s; j >= e; j--) {
-					System.out.printf("%d * %d = %2d", j, i, j * i);
-					if(j != e) {
-						System.out.print("   ");
-					}
-				}
-				System.out.println();
-			}
-			else {
-				for(int j = s; j <= e; j++) {
-					System.out.printf("%d * %d = %2d", j, i, j * i);
-					if(j != e) {
-						System.out.print("   ");
-					}
-				}
-				System.out.println();
-			}
-			
-			
-		}
 
+		// 출력
+		for (int i = 1; i < 10; i++) {
+			// if -> s 가 e 보다 큰 경우 / else -> s 가 e 보다 작은 경우
+			if (s > e) {
+				for (int j = s; j >= e; j--) {
+					System.out.printf("%d * %d = %2d", j, i, j * i);
+					if (j != e) {
+						System.out.print("   ");
+					}
+				}
+				System.out.println();
+			} else {
+				for (int j = s; j <= e; j++) {
+					System.out.printf("%d * %d = %2d", j, i, j * i);
+					if (j != e) {
+						System.out.print("   ");
+					}
+				}
+				System.out.println();
+			}
+		}
 	}
 }
