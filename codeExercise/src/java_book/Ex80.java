3ac0466816781ea5f64cc8475b42c3ba3d3b16db
No4h <no4h1209@gmail.com>
2025-01-13 11:17:40 +0900

    No4h

--- codeExercise/src/java_book/Ex80.java
+++ codeExercise/src/java_book/Ex80.java
@@ -1,41 +1,76 @@
+// 계산기
 package java_book;
 
 import java.awt.BorderLayout;
 import java.awt.Button;
-import java.awt.Color;
 import java.awt.Frame;
+import java.awt.GridLayout;
 import java.awt.Panel;
 import java.awt.TextField;
 
 public class Ex80 {
-	private Frame f;
-	private Panel p;
-	private Button ok, cancel;
+	private Frame frame;
+	private Panel p1, p2;
+	private Button n1, n2, n3, n4, n5, n6, n7, n8, n9, n0, plus, min, mul, div, equalButton, none, clear;
 	private TextField tf1;
 	
 	public Ex80() {
-		f = new Frame("Panel example");
-		ok = new Button("전송");
-		cancel = new Button("삭제");
-		
+		frame = new Frame("Calculator example");
 		tf1 = new TextField("Input...");
-		p = new Panel();
+		p1 = new Panel();
+		p2 = new Panel();
+		n1 = new Button("1");
+		n2 = new Button("2");
+		n3 = new Button("3");
+		n4 = new Button("4");
+		n5 = new Button("5");
+		n6 = new Button("6");
+		n7 = new Button("7");
+		n8 = new Button("8");
+		n9 = new Button("9");
+		n0 = new Button("0");
+		plus = new Button("+");
+		min = new Button("-");
+		mul = new Button("x");
+		div = new Button("÷");
+		equalButton = new Button("=");
+		none = new Button(" ");
+		clear = new Button("CLEAR");
 	}
-	
+
 	public void startFrame() {
-		f.add(tf1, BorderLayout.NORTH);
-		p.setBackground(Color.cyan);
-		p.add(ok);
-		p.add(cancel);
-		f.add(p, BorderLayout.CENTER);
+		p2.setLayout(new GridLayout(4, 4));
+		p2.add(n7);
+		p2.add(n8);
+		p2.add(n9);
+		p2.add(plus);
+		p2.add(n4);
+		p2.add(n5);
+		p2.add(n6);
+		p2.add(min);
+		p2.add(n1);
+		p2.add(n2);
+		p2.add(n3);
+		p2.add(mul);
+		p2.add(none);
+		p2.add(n0);
+		p2.add(equalButton);
+		p2.add(div);
 		
-		f.setSize(400, 400);;
-		f.setVisible(true);
+		p1.setLayout(new BorderLayout());
+		p1.add(tf1, BorderLayout.CENTER);
+		p1.add(clear, BorderLayout.EAST);
+		
+		frame.add(p1, BorderLayout.NORTH);
+		frame.add(p2, BorderLayout.CENTER);
+		
+		frame.setSize(300, 450);
+		frame.setVisible(true);
 	}
-	
+
 	public static void main(String[] arsg) {
-		Ex80 g = new Ex80();
-		g.startFrame();
+		Ex80 cal = new Ex80();
+		cal.startFrame();
 	}
-	
+
 }
