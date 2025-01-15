535eae200db67ef7c9b81064435b2589b4ca11eb
No4h <no4h1209@gmail.com>
2025-01-15 16:32:23 +0900

    No4h

--- codeExercise/src/java_backjoon/Ex34.java
+++ codeExercise/src/java_backjoon/Ex34.java
@@ -1,9 +1,25 @@
 package java_backjoon;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+//import java.util.NavigableSet;
+import java.util.TreeSet;
 
 public class Ex34 {
-	public static void main(String[] args) throws IOException{
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		TreeSet<String> word = new TreeSet<String>();
+		for (int i = 0; i < N; i++) {
+			word.add(br.readLine());
+		}
+		
+		
+//		NavigableSet<String> nWord = word.descendingSet();
+//		NavigableSet<String> nWords = nWord.descendingSet();
+//		for (String str : nWords) {
+//			System.out.println(str);
+//		}
 	}
 }
---
+++ codeExercise/src/java_jungol/Ex27.java
@@ -0,0 +1,8 @@
+// 정올-1840-치즈
+package java_jungol;
+
+public class Ex27 {
+	public static void main(String[] arsg) {
+		
+	}
+}
