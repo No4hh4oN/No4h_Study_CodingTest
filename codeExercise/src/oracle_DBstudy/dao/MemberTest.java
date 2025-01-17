90549a86138e12f4c22ce8a945dfaaa7946d1e85
No4h <no4h1209@gmail.com>
2025-01-17 14:42:42 +0900

    No4h

--- codeExercise/src/oracle_DBstudy/dao/MemberTest.java
+++ codeExercise/src/oracle_DBstudy/dao/MemberTest.java
@@ -1,12 +1,18 @@
 package oracle_DBstudy.dao;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MemberTest {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input ename...? " );
+		String nameString = sc.nextLine();
+		sc.close();
+		
 		MemberDAO dao = new MemberDAO();
-		ArrayList<MemberVo> list = dao.list();
+		ArrayList<MemberVo> list = dao.list(nameString);
 
 		for (int i = 0; i < list.size(); i++) {
 			MemberVo data = (MemberVo) list.get(i);
@@ -20,4 +26,3 @@ public class MemberTest {
 	}
 
 }
-
