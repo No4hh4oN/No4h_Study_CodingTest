6fe9b69462a2d76c796f31f9dacd3eb044b48e13
No4h <no4h1209@gmail.com>
2025-01-21 09:16:40 +0900

    No4h

---
+++ codeExercise/src/java_jungol/Ex30.java
@@ -0,0 +1,16 @@
+package java_jungol;
+
+import java.util.Scanner;
+
+public class Ex30 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		circle(n1);
+	}
+	
+	static void circle(int n) {
+		double d = n * n * 3.14;
+		System.out.printf("%.2f", d);
+	}
+}
