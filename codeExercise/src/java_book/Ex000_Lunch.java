0e29e8d6fda7fc7cdf7f8f4acf9784179ef3f1d0
No4h <no4h1209@gmail.com>
2025-01-21 12:42:33 +0900

    No4h

--- codeExercise/src/java_book/Ex000_Lunch.java
+++ codeExercise/src/java_book/Ex000_Lunch.java
@@ -6,9 +6,8 @@ import java.util.Scanner;
 
 public class Ex000_Lunch {
 	public static void main(String[] args) throws IOException, InterruptedException {
-		String[] lunchOptions = { "맘스터치", "토리코코로", "몽키파스타", "편의점", "홍원", "주떡", "돈까스", "청와삼대", "세상만사감자탕(뼈해장국)", "바글바글",
-				"꿈떡", "중국관", "창부리또", "하늘지기", "쌀국수공방", "스마일닭갈비", "다람이임자탕(채식)", "최고집해물찜칼국수", "후참잘", "신룽푸마라탕",
-				"담터추어탕(잔치국수, 비빔국수)", "담터쭈꾸미(쭈꾸미+비빔밥+피자+샐러드세트)", "배달", "우육면" };
+		String[] lunchOptions = { "맘스터치", "토리코코로", "몽키파스타", "편의점", "홍원", "주떡", "돈까스", "청와삼대", "세상만사감자탕(뼈해장국)", "꿈떡",
+				"중국관", "창부리또", "쌀국수공방", "다람이임자탕(채식)", "최고집해물찜칼국수", "배달" };
 
 		Random random = new Random();
 		Scanner scanner = new Scanner(System.in);
@@ -30,4 +29,4 @@ public class Ex000_Lunch {
 
 		System.out.println("\n결정된 메뉴: " + lunchOptions[random.nextInt(lunchOptions.length)]);
 	}
-} 
+}
