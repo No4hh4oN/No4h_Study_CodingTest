d7279dd85d51d4d9f3ff12e3c67882dd156fe39a
No4h <no4h1209@gmail.com>
2025-03-02 23:08:48 +0900

    정렬

--- codeExercise/src/java_backjoon/Ex43.java
+++ codeExercise/src/java_backjoon/Ex43.java
@@ -8,58 +8,57 @@ import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class Ex43 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        ArrayList<Integer> arrayList = new ArrayList<Integer>();
-        int N = Integer.parseInt(br.readLine());
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            String[] input = br.readLine().split(" ");
-            int order = Integer.parseInt(input[0]);
+		for (int i = 0; i < N; i++) {
+			String[] input = br.readLine().split(" ");
+			int order = Integer.parseInt(input[0]);
 
-
-            switch (order) {
-                case 1: {
-                    int addNum = Integer.parseInt(input[1]);
-                    arrayList.add(addNum);
-                    break;
-                }
-                case 2: {
-                    if (arrayList.isEmpty()) {
-                        bw.write("-1\n");
-                    } else {
-                        bw.write(arrayList.get(arrayList.size() - 1) + "\n");
-                        arrayList.remove(arrayList.size() - 1);
-                    }
-                    break;
-                }
-                case 3: {
-                    bw.write(arrayList.size() + "\n");
-                    break;
-                }
-                case 4: {
-                    if (arrayList.isEmpty()) {
-                        bw.write("1\n");
-                    } else {
-                        bw.write("0\n");
-                    }
-                    break;
-                }
-                case 5: {
-                    if (arrayList.isEmpty()) {
-                        bw.write("-1\n");
-                    } else {
-                        bw.write(arrayList.get(arrayList.size() - 1) + "\n");
-                    }
-                    break;
-                }
-                default:
-                    break;
-            }
-            bw.flush();
-        }
-        bw.close();
-    }
+			switch (order) {
+			case 1: {
+				int addNum = Integer.parseInt(input[1]);
+				arrayList.add(addNum);
+				break;
+			}
+			case 2: {
+				if (arrayList.isEmpty()) {
+					bw.write("-1\n");
+				} else {
+					bw.write(arrayList.get(arrayList.size() - 1) + "\n");
+					arrayList.remove(arrayList.size() - 1);
+				}
+				break;
+			}
+			case 3: {
+				bw.write(arrayList.size() + "\n");
+				break;
+			}
+			case 4: {
+				if (arrayList.isEmpty()) {
+					bw.write("1\n");
+				} else {
+					bw.write("0\n");
+				}
+				break;
+			}
+			case 5: {
+				if (arrayList.isEmpty()) {
+					bw.write("-1\n");
+				} else {
+					bw.write(arrayList.get(arrayList.size() - 1) + "\n");
+				}
+				break;
+			}
+			default:
+				break;
+			}
+			bw.flush();
+		}
+		bw.close();
+	}
 }
