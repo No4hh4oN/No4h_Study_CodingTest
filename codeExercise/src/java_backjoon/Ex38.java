04a06a42afc63f0a22a9079bf59f076cd4643840
No4h <no4h1209@gmail.com>
2025-03-04 13:48:08 +0900

    백준 - 좌표압축

--- codeExercise/src/java_backjoon/Ex38.java
+++ codeExercise/src/java_backjoon/Ex38.java
@@ -8,21 +8,69 @@ public class Ex38 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-
+        
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         
+        int[] cloneArr = arr.clone();
+        Arrays.sort(cloneArr);
+        
         HashMap<Integer,Integer> map = new HashMap<>();
+        int rank = 0;
         
-        int n = 0;
-        for(int arrs : arr) {
-        	map.put(n++, arrs);
+        for(int i : cloneArr) {
+        	if(!map.containsKey(i)) {
+        		map.put(i, rank++);
+        	}
         }
         
-        for (Integer i : map.keySet()) {
-            System.out.println(map.get(i));
+        StringBuilder sb = new StringBuilder();
+        for(int i : arr) {
+        	sb.append(map.get(i)).append(" ");
         }
+        
+        System.out.println(sb);
     }
 }
+
+//
+//import java.io.*;
+//import java.util.*;
+//
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int N = Integer.parseInt(br.readLine());
+//        int[] arr = new int[N];
+//
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        for (int i = 0; i < N; i++) {
+//            arr[i] = Integer.parseInt(st.nextToken());
+//        }
+//
+//        // 원본 배열을 복사하여 정렬된 리스트 생성
+//        int[] sortedArr = arr.clone();
+//        Arrays.sort(sortedArr);
+//
+//        // HashMap을 사용하여 각 값의 좌표 압축된 순위 저장
+//        Map<Integer, Integer> map = new HashMap<>();
+//        int rank = 0;
+//
+//        for (int num : sortedArr) {
+//            if (!map.containsKey(num)) {
+//                map.put(num, rank++);
+//            }
+//        }
+//
+//        // 결과를 StringBuilder에 저장 후 한 번에 출력하여 시간 절약
+//        StringBuilder sb = new StringBuilder();
+//        for (int num : arr) {
+//            sb.append(map.get(num)).append(" ");
+//        }
+//
+//        System.out.println(sb);
+//    }
+//}
