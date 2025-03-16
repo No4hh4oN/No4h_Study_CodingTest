2faeb4eadb535e5e517d46e71f4fde660a3d5f4a
No4h <no4h1209@gmail.com>
2025-03-16 22:24:48 +0900

    No4h (버퍼드리더, 라이터 / 어레이리스트 관련 기본 코드)

---
+++ codeExercise/src/java_backjoon/Ex51.java
@@ -0,0 +1,35 @@
+package java_backjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Ex51 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String[] N = br.readLine().split(" ");
+		int N1 = Integer.parseInt(N[0]);
+		int N2 = Integer.parseInt(N[1]);
+
+		String[] arr1 = br.readLine().split(" ");
+
+		ArrayList<Integer> set = new ArrayList<Integer>();
+
+		Integer[] resultArray = set.toArray(new Integer[0]);
+
+		Arrays.sort(resultArray);
+		for (int num : resultArray) {
+			bw.write(num + " ");
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
