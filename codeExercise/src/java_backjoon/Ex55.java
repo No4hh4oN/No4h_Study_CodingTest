ea6a868d6b942f13d5120d5103a17bb4b4c1c79e
No4h <no4h1209@gmail.com>
2025-04-06 23:47:39 +0900

    No4h

---
+++ codeExercise/src/java_backjoon/Ex55.java
@@ -0,0 +1,12 @@
+package java_backjoon;
+
+import java.util.Scanner;
+
+public class Ex55 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		sc.close();
+	}
+}
