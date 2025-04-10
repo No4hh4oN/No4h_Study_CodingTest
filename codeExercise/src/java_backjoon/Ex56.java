aef722943016c825b9ba5092ef9f7a75c24d29a6
No4h <no4h1209@gmail.com>
2025-04-10 16:52:16 +0900

    백준 - 수학은 체육과목입니다.

--- codeExercise/src/java_backjoon/Ex56.java
+++ codeExercise/src/java_backjoon/Ex56.java
@@ -6,7 +6,8 @@ public class Ex56 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		
+		long N = sc.nextLong();
+		System.out.println(N * 4);
 		sc.close();
 	}
 }
