0110740a15d16fc91542feb6471a95a7760a0530
No4h <no4h1209@gmail.com>
2025-04-10 14:18:39 +0900

    백준 - 제로

--- codeExercise/src/java_backjoon/Ex55.java
+++ codeExercise/src/java_backjoon/Ex55.java
@@ -1,12 +1,31 @@
 package java_backjoon;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Ex55 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		
+
+		Stack<Integer> stack = new Stack<Integer>();
+
+		int N = sc.nextInt();
+		int total = 0;
+		for (int i = 0; i < N; i++) {
+			int num = sc.nextInt();
+			if (num == 0) {
+				total -= stack.get(stack.size() - 1);
+				stack.remove(stack.size() - 1);
+
+			} else {
+				total += num;
+				stack.add(num);
+
+			}
+		}
+
+		System.out.println(total);
+
 		sc.close();
 	}
 }
---
+++ codeExercise/src/java_backjoon/Ex56.java
@@ -0,0 +1,12 @@
+package java_backjoon;
+
+import java.util.Scanner;
+
+public class Ex56 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		sc.close();
+	}
+}
